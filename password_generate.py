b3d2424e5453301a281c4160dd157a4403ac082b
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2022-10-01 05:59:17 -0300

    clean

--- password_generate.py
+++ password_generate.py
@@ -1,12 +1,11 @@
-import numbers
 import random
 
 lower_case = 'abcdefghijklmnopqrstuvwxyz'
 upper_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-numbers = '0123456789'
+number = '0123456789'
 special_characters = '!@#$%^&*()_+'
 
-Use = lower_case + upper_case + numbers + special_characters
+Use = lower_case + upper_case + number + special_characters
 length = 8
 
 password = ''.join(random.sample(Use, length))
